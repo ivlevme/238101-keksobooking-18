36e6f97236f173d426ab6536ccbf0d7f257b12a2
ivlev <ivlev.me@gmail.com>
2019-09-24 13:59:14 +0300

    Исправление замечаний

--- js/main.js
+++ js/main.js
@@ -1,80 +1,109 @@
 'use strict';
 
-var TYPE_HOME = ['palace', 'flat', 'house', 'bungalo'];
-var TIMES = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+var TYPES_HOME = [
+  'palace',
+  'flat',
+  'house',
+  'bungalo'
+];
+var TIMES_CHECK = [
+  '12:00',
+  '13:00',
+  '14:00'
+];
+var FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
+var PHOTOS = [
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
-  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var PIN_SIZES = {
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
+
+var avatar = {
+  src: 'img/avatars/user',
+  fitstNumber: '0',
+  expansion: '.png'
+};
+var offer = {
+  title: 'Заголовок предложение - ',
+  description: 'Описание - '
+};
+var pinSize = {
   width: 75,
   height: 87
 };
 
-var generateData = function (count) {
-  var datas = [];
+var getPinsMocks = function (count) {
+  var pins = [];
   for (var i = 0; i < count; i++) {
+    var coordinate = {
+      x: generateRandomNumber(0, mapSizes.width - pinSize.width),
+      y: generateRandomNumber(130, 630)
+    };
     var element = {
       author: {
-        avatar: 'img/avatars/user' + '0' + (i + 1) + '.png',
+        avatar: avatar.src + avatar.fitstNumber + (i + 1) + avatar.expansion,
       },
 
       offer: {
-        title: 'Заголовок предложение - ' + (i + 1),
-        address: generateRandomNumber(100, 600).toString() + ', ' +
-          generateRandomNumber(100, 600).toString(),
-        price: (i + 1) + '000',
-        type: TYPE_HOME[generateRandomNumber(0, TYPE_HOME.length - 1)],
+        title: offer.title + (i + 1),
+        address: coordinate.x + ', ' + coordinate.y,
+        price: generateRandomNumber(1000, 10000),
+        type: TYPES_HOME[generateRandomNumber(0, TYPES_HOME.length - 1)],
         rooms: generateRandomNumber(1, 4),
         guests: generateRandomNumber(1, 10),
-        checkin: TIMES[generateRandomNumber(0, TIMES.length - 1)],
-        checkout: TIMES[generateRandomNumber(0, TIMES.length - 1)],
-        features: generateLine(FEATURES, generateRandomNumber(1, FEATURES.length)),
-        description: 'Описание - ' + (i + 1),
-        photos: generateLine(PHOTOS, generateRandomNumber(1, PHOTOS.length))
+        checkin: TIMES_CHECK[generateRandomNumber(0, TIMES_CHECK.length - 1)],
+        checkout: TIMES_CHECK[generateRandomNumber(0, TIMES_CHECK.length - 1)],
+        features: generateOptions(FEATURES, generateRandomNumber(1, FEATURES.length)),
+        description: offer.description + (i + 1),
+        photos: generateOptions(PHOTOS, generateRandomNumber(1, PHOTOS.length))
       },
 
       location: {
-        x: generateRandomNumber(0, mapSizes.width - PIN_SIZES.width),
-        y: generateRandomNumber(130, 630)
+        x: coordinate.x,
+        y: coordinate.y
       }
     };
-    datas.push(element);
+    pins.push(element);
   }
-  return datas;
+  return pins;
 };
 
 var generateRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var generateLine = function (elements, count) {
-  var dataLine = [];
+var generateOptions = function (elements, count) {
+  var options = [];
   var featuresElement = elements.slice();
   for (var i = 0; i < count; i++) {
     var indexCurrentElement = generateRandomNumber(0, featuresElement.length - 1);
-    dataLine[i] = featuresElement[indexCurrentElement];
+    options[i] = featuresElement[indexCurrentElement];
     featuresElement.splice(indexCurrentElement, 1);
   }
-  return dataLine;
+  return options;
 };
 
-var generatePins = function (data) {
+var generatePins = function (pins) {
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < Object.keys(data).length; i++) {
+  for (var i = 0; i < Object.keys(pins).length; i++) {
     var pin = pinTemplate.cloneNode(true);
-    pin.style.left = data[i].location.x.toString() + 'px';
-    pin.style.top = data[i].location.y.toString() + 'px';
+    pin.style.left = pins[i].location.x.toString() + 'px';
+    pin.style.top = pins[i].location.y.toString() + 'px';
+
+
     var imgPin = pin.querySelector('img');
-    imgPin.src = data[i].author.avatar.toString();
-    imgPin.alt = data[i].offer.title.toString();
+    imgPin.src = pins[i].author.avatar;
+    imgPin.alt = pins[i].offer.title;
     fragment.appendChild(pin);
   }
   return fragment;
-};
-
-var insertPins = function (fragment, location) {
-  location.appendChild(fragment);
 };
 
 var mapOverlay = document.querySelector('.map__overlay');
@@ -83,14 +112,13 @@ var mapSizes = {
   height: mapOverlay.clientHeight
 };
 
-var fakeDatas = generateData(8);
+var mapContainer = document.querySelector('.map');
+mapContainer.classList.remove('map--faded');
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
-
+var pinsMocks = getPinsMocks(8);
 var pinTemplate = document.querySelector('#pin')
     .content
     .querySelector('.map__pin');
-var pinsFragment = generatePins(fakeDatas);
-var mapPins = document.querySelector('.map__pins');
-insertPins(pinsFragment, mapPins);
+var pinsFragment = generatePins(pinsMocks);
+var mapOverlayContainer = mapContainer.querySelector('.map__pins');
+mapOverlayContainer.appendChild(pinsFragment);
