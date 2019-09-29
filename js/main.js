4fa2cd8e5fca4d14c4630d8f0a65b80d5a3658a0
ivlev <ivlev.me@gmail.com>
2019-09-29 19:29:59 +0300

    выполение дз

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ var PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
 ];
+var ENTER_KEYCODE = 13;
 
 var avatar = {
   src: 'img/avatars/user',
@@ -41,7 +42,7 @@ var pinSize = {
 var limitsY = {
   min: 130,
   max: 630
-}
+};
 
 var getPinsMocks = function (count) {
   var pins = [];
@@ -110,6 +111,71 @@ var generatePins = function (pins) {
   return fragment;
 };
 
+var activationPage = function () {
+  mapContainer.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+  var adFormFieldsets = adForm.querySelectorAll('fieldset');
+  for (var i = 0; i < adFormFieldsets.length; i++) {
+    adFormFieldsets[i].removeAttribute('disabled');
+  }
+
+  var mapFilters = mapContainer.querySelector('.map__filters');
+  var mapFiltersSelects = mapFilters.querySelectorAll('select');
+  for (i = 0; i < mapFiltersSelects.length; i++) {
+    mapFiltersSelects[i].removeAttribute('disabled');
+  }
+  var mapFiltersFieldset = mapFilters.querySelector('fieldset');
+  mapFiltersFieldset.removeAttribute('disabled');
+
+  var PinLocation = {
+    x: defaultPinLocation.x + pinSize.width,
+    y: defaultPinLocation.y + pinSize.height
+  };
+  inputAddress.value = PinLocation.x + ', ' + PinLocation.y;
+  mapOverlayContainer.appendChild(pinsFragment);
+};
+
+var customValidation = function () {
+  var inputRooms = adForm.querySelector('select[name="rooms"]');
+  var inputCapacity = adForm.querySelector('select[name="capacity"]');
+  switch (inputRooms.value) {
+    case '1':
+      if (inputCapacity.value === inputCapacity.options[2].value) {
+        inputRooms.setCustomValidity('');
+      } else {
+        inputRooms.setCustomValidity('1 комната — «для 1 гостя»');
+      }
+      break;
+    case '2':
+      if (inputCapacity.value === inputCapacity.options[1].value ||
+          inputCapacity.value === inputCapacity.options[2].value) {
+        inputRooms.setCustomValidity('');
+      } else {
+        inputRooms.setCustomValidity('2 комнаты — «для 2 гостей» или «для 1 гостя»');
+      }
+      break;
+    case '3':
+      if (inputCapacity.value === inputCapacity.options[0].value ||
+          inputCapacity.value === inputCapacity.options[1].value ||
+          inputCapacity.value === inputCapacity.options[2].value) {
+        inputRooms.setCustomValidity('');
+      } else {
+        inputRooms.setCustomValidity('3 комнаты — «для 3 гостей», «для 2 гостей» или «для 1 гостя»');
+      }
+      break;
+    case '100':
+      if (inputCapacity.value === inputCapacity.options[3].value) {
+        inputRooms.setCustomValidity('');
+      } else {
+        inputRooms.setCustomValidity('100 комнат — «не для гостей');
+      }
+      break;
+
+    default:
+      break;
+  }
+};
+
 var mapOverlay = document.querySelector('.map__overlay');
 var mapSizes = {
   width: mapOverlay.clientWidth,
@@ -117,7 +183,6 @@ var mapSizes = {
 };
 
 var mapContainer = document.querySelector('.map');
-mapContainer.classList.remove('map--faded');
 
 var pinsMocks = getPinsMocks(8);
 var pinTemplate = document.querySelector('#pin')
@@ -125,4 +190,26 @@ var pinTemplate = document.querySelector('#pin')
     .querySelector('.map__pin');
 var pinsFragment = generatePins(pinsMocks);
 var mapOverlayContainer = mapContainer.querySelector('.map__pins');
-mapOverlayContainer.appendChild(pinsFragment);
+
+var mapPinMain = mapContainer.querySelector('.map__pin--main');
+var defaultPinLocation = {
+  x: parseInt(mapPinMain.style.left.substring(0, mapPinMain.style.left.length - 2), 10),
+  y: parseInt(mapPinMain.style.top.substring(0, mapPinMain.style.left.length - 2), 10)
+};
+var notice = document.querySelector('.notice');
+var adForm = notice.querySelector('.ad-form');
+var inputAddress = adForm.querySelector('input[name="address"]');
+inputAddress.value = defaultPinLocation.x + ', ' + defaultPinLocation.y;
+mapPinMain.addEventListener('mousedown', function () {
+  activationPage();
+});
+mapPinMain.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    activationPage();
+  }
+});
+
+var adFormSubmit = adForm.querySelector('.ad-form__submit');
+adFormSubmit.addEventListener('click', function () {
+  customValidation();
+});
