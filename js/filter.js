01e7033c700d4d7ce4c28113d0c85e281ea03e57
ivlev <ivlev.me@gmail.com>
2020-03-29 14:51:32 +0300

    structurization

--- js/filter.js
+++ js/filter.js
@@ -2,6 +2,7 @@
 
 (function () {
   var EMPTY_QUANTITY = 0;
+  var ANY_QUANTITY = 'any';
 
   var HousingRooms = {
     ONE: 1,
@@ -26,111 +27,103 @@
     HIGH: 50000
   };
 
-  var TypeAccommodation = window.setup.TypeAccommodation;
   var mapFilterContainer = window.setup.mapFilterContainer;
 
-  var OfferPinSchema = window.card.OfferPinSchema;
-
-  var pointsFilter = [];
-
-  var selectHousingType = mapFilterContainer.querySelector('#housing-type');
-  var selectHousingPrice = mapFilterContainer.querySelector('#housing-price');
-  var selectHousingRooms = mapFilterContainer.querySelector('#housing-rooms');
-  var selectHousingGuests = mapFilterContainer.querySelector('#housing-guests');
-  var selectHousingFeatures = mapFilterContainer.querySelector('#housing-features');
-
-  pointsFilter.push(selectHousingType);
-  pointsFilter.push(selectHousingPrice);
-  pointsFilter.push(selectHousingRooms);
-  pointsFilter.push(selectHousingGuests);
-  pointsFilter.push(selectHousingFeatures);
+  var categorizedPins = {};
 
 
-  var verify = function (data) {
+  var mapFilterForm = mapFilterContainer.querySelector('.map__filters');
+  var selectHousingType = mapFilterForm.querySelector('#housing-type');
+  var selectHousingPrice = mapFilterForm.querySelector('#housing-price');
+  var selectHousingRooms = mapFilterForm.querySelector('#housing-rooms');
+  var selectHousingGuests = mapFilterForm.querySelector('#housing-guests');
+  var selectHousingFeatures = mapFilterForm.querySelector('#housing-features');
+  var inputHousingFeatures = selectHousingFeatures.querySelectorAll('input[type="checkbox"');
+
+
+  window.filter = function (data) {
+    categorizePins(data);
     data = checkHousingType(selectHousingType.value, data);
     var finalPins = data.slice(0, 5);
     return finalPins;
   };
-  var checkHousingType = function (selectedType, data) {
-    switch (selectedType) {
-      case TypeAccommodation.PALACE:
-        data = filterCompare(data, TypeAccommodation.PALACE, OfferPinSchema.TYPE);
-        break;
 
-      case TypeAccommodation.FLAT:
-        data = filterCompare(data, TypeAccommodation.FLAT, OfferPinSchema.TYPE);
-        break;
+  var categorizePins = function (data) {
+    Array.from(mapFilterForm.children).forEach(function (child) {
+      initializeStructureCategories(child, data);
+    });
 
-      case TypeAccommodation.HOUSE:
-        data = filterCompare(data, TypeAccommodation.HOUSE, OfferPinSchema.TYPE);
-        break;
+    data.forEach(function (pin) {
+      categorizedPins[selectHousingType.id][pin.offer.type].push(pin);
 
-      case TypeAccommodation.BUNGALO:
-        data = filterCompare(data, TypeAccommodation.BUNGALO, OfferPinSchema.TYPE);
-        break;
+      if (pin.offer.rooms >= HousingRooms.ONE && pin.offer.rooms <= HousingRooms.THREE) {
+        categorizedPins[selectHousingRooms.id][pin.offer.rooms].push(pin);
+      }
+
+      if (pin.offer.guests >= HousingGuests.NOTGUESTS && pin.offer.guests <= HousingGuests.TWO) {
+        categorizedPins[selectHousingGuests.id][pin.offer.rooms].push(pin);
+      }
+
+      sortHousingPrice(pin);
+      sortHousingFeatures(pin);
+    });
+
+    return categorizedPins;
+  };
+
+  var initializeStructureCategories = function (selectElement, data) {
+    categorizedPins[selectElement.id] = {};
+
+    var arraySelectElement = (selectElement === selectHousingFeatures) ? Array.from(inputHousingFeatures) : Array.from(selectElement.options);
+
+    arraySelectElement.forEach(function (item) {
+      categorizedPins[selectElement.id][item.value] = (item.value === ANY_QUANTITY) ? data : [];
+    });
+  };
+
+  var sortHousingPrice = function (pin) {
+    if (pin.offer.price < Price.LOW) {
+      return categorizedPins[selectHousingPrice.id][HousingPrice.LOW].push(pin);
     }
+
+    if (pin.offer.price >= Price.LOW && pin.offer.price <= Price.HIGH) {
+      return categorizedPins[selectHousingPrice.id][HousingPrice.MIDDLE].push(pin);
+    }
+
+    return categorizedPins[selectHousingPrice.id][HousingPrice.HIGH].push(pin);
+  };
+
+  var sortHousingFeatures = function (pin) {
+    pin.offer.features.forEach(function (feature) {
+      categorizedPins[selectHousingFeatures.id][feature].push(pin);
+    });
+  };
+
+  var checkHousingType = function (selectedType, data) {
+    data = categorizedPins[selectHousingType.id][selectedType];
     return checkHousingPrice(selectHousingPrice.value, data);
   };
 
   var checkHousingPrice = function (selectedPrice, data) {
-    switch (selectedPrice) {
-      case HousingPrice.LOW:
-        data = data.filter(function (item) {
-          return item.offer.price < Price.LOW;
-        });
-        break;
+    data = data.filter(function (pin) {
+      return categorizedPins[selectHousingPrice.id][selectedPrice].includes(pin);
+    });
 
-      case HousingPrice.MIDDLE:
-        data = data.filter(function (item) {
-          return item.offer.price >= Price.LOW && item.offer.price <= Price.HIGH;
-        });
-        break;
-
-      case HousingPrice.HIGH:
-        data = data.filter(function (item) {
-          return item.offer.price > Price.HIGH;
-        });
-        break;
-    }
     return checkHousingRooms(selectHousingRooms.value, data);
   };
 
   var checkHousingRooms = function (selectedRooms, data) {
-    selectedRooms = parseInt(selectedRooms, 10);
-
-    switch (selectedRooms) {
-      case HousingRooms.ONE:
-        data = filterCompare(data, HousingRooms.ONE, OfferPinSchema.ROOMS);
-        break;
-
-      case HousingRooms.TWO:
-        data = filterCompare(data, HousingRooms.TWO, OfferPinSchema.ROOMS);
-        break;
-
-      case HousingRooms.THREE:
-        data = filterCompare(data, HousingRooms.THREE, OfferPinSchema.ROOMS);
-        break;
-    }
+    data = data.filter(function (pin) {
+      return categorizedPins[selectHousingRooms.id][selectedRooms].includes(pin);
+    });
 
     return checkHousingGuests(selectHousingGuests.value, data);
   };
 
   var checkHousingGuests = function (selectedGuests, data) {
-    selectedGuests = parseInt(selectedGuests, 10);
-
-    switch (selectedGuests) {
-      case HousingGuests.ONE:
-        data = filterCompare(data, HousingGuests.ONE, OfferPinSchema.GUESTS);
-        break;
-
-      case HousingGuests.TWO:
-        data = filterCompare(data, HousingGuests.TWO, OfferPinSchema.GUESTS);
-        break;
-
-      case HousingGuests.NOTGUESTS:
-        data = filterCompare(data, HousingGuests.NOTGUESTS, OfferPinSchema.GUESTS);
-        break;
-    }
+    data = data.filter(function (pin) {
+      return categorizedPins[selectHousingGuests.id][selectedGuests].includes(pin);
+    });
 
     return checkHousingFeatures(data);
   };
@@ -147,17 +140,5 @@
     }
 
     return data;
-  };
-
-  var filterCompare = function (data, element, kind) {
-    data = data.filter(function (item) {
-      return item.offer[kind] === element;
-    });
-    return data;
-  };
-
-  window.filter = {
-    verify: verify,
-    pointsFilter: pointsFilter
   };
 })();
